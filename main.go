b4d574083bf8f2d7c58e782c7f60527493256765
snaskar05 <santanu.naskar@nirvanatech.com>
2023-09-13 15:51:16 +0530

    Init commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "sync"
+
+var g sync.WaitGroup
+
+func main() {
+	go try1()
+	go try2()
+	g.Wait()
+	print("All coroutines finished")
+}
+
+func try2() {
+	g.Add(1)
+	defer print("Go2")
+	print("Hello World2!!")
+	g.Done()
+}
+
+func try1() {
+	g.Add(1)
+	defer print("Go1")
+	print("Hello World1!!")
+	g.Done()
+}
